320d58a68f7bc13ac7a2f99951603f326594a344
MaxAndreev27 <maxandreev27@gmail.com>
2024-11-03 21:45:13 +0200

    Add lowdb npm

--- chap2-todo/src/todoItem.ts
+++ chap2-todo/src/todoItem.ts
@@ -1,5 +1,8 @@
 export class TodoItem {
-    constructor(public id: number, public task: string, public complete: boolean = false) {
+
+    constructor(public id: number,
+                public task: string,
+                public complete: boolean = false) {
         // no statements required
     }
 
