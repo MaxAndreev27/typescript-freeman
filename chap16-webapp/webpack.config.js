3176cba9fd4f5079462206737e7291b5179a89b3
MaxAndreev27 <maxandreev27@gmail.com>
2024-11-26 20:47:57 +0200

    WebbApp: webpack, bootstrap

---
+++ chap16-webapp/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const dist = path.resolve(__dirname, "build");
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: "./src/index.ts",
+  output: {
+    path: dist,
+    filename: "bundle.js"
+  },
+  resolve: { extensions: [".ts", ".tsx", ".js", ".css"] },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+    ]
+  },
+  devServer: {
+    static: "./assets",
+    port: 4500
+  }
+};
