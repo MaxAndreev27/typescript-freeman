65dee1b56536640b4e74c6ec985fbbb70eaf8ded
MaxAndreev27 <maxandreev27@gmail.com>
2024-11-30 21:13:19 +0200

    Angular app ver2 ready

--- chap18-angularapp/src/app/app.module.ts
+++ chap18-angularapp/src/app/app.module.ts
@@ -1,6 +1,5 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from "@angular/forms";
@@ -9,10 +8,22 @@ import { ProductItem } from './productItem.component';
 import { CategegoryList } from "./categoryList.component";
 import { Header } from "./header.component";
 import { ProductList } from "./productList.component";
+import { RouterModule } from "@angular/router"
+import { OrderDetails } from "./orderDetails.component";
+import { Summary } from "./summary.component";
+
+const routes = RouterModule.forRoot([
+  { path: "products", component: ProductList },
+  { path: "order", component: OrderDetails},
+  { path: "summary/:id", component: Summary},
+  { path: "", redirectTo: "/products", pathMatch: "full"}
+]);
 
 @NgModule({
-  declarations: [AppComponent, ProductItem, CategegoryList, Header, ProductList],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, DataModelModule],
+  declarations: [AppComponent,  ProductItem, CategegoryList,
+    Header, ProductList, OrderDetails, Summary],
+  imports: [BrowserModule, AppRoutingModule, FormsModule,
+    DataModelModule, routes],
   providers: [],
   bootstrap: [AppComponent]
 })
--- chap18-angularapp/src/app/productList.component.ts
+++ chap18-angularapp/src/app/productList.component.ts
@@ -1,35 +1,36 @@
 import { Component } from "@angular/core";
 import { DataSource } from './data/dataSource';
 import { Product } from './data/entities';
+import { Router } from "@angular/router";
 
 @Component({
-    selector: "product-list",
-    templateUrl: "./productList.component.html"
+  selector: "product-list",
+  templateUrl: "./productList.component.html"
 })
 export class ProductList {
-    selectedCategory = "All";
+  selectedCategory = "All";
 
-    constructor(public dataSource: DataSource) {}
+  constructor(public dataSource: DataSource, private router: Router) {}
 
-    get products(): Product[] {
-        return this.dataSource.getProducts("id",
-            this.selectedCategory === "All"
-              ? undefined : this.selectedCategory);
-    }
+  get products(): Product[] {
+    return this.dataSource.getProducts("id",
+      this.selectedCategory === "All"
+        ? undefined : this.selectedCategory);
+  }
 
-    get categories(): string[] {
-        return ["All", ...this.dataSource.getCategories()];
-    }
+  get categories(): string[] {
+    return ["All", ...this.dataSource.getCategories()];
+  }
 
-    handleCategorySelect(category: string) {
-        this.selectedCategory = category;
-    }
+  handleCategorySelect(category: string) {
+    this.selectedCategory = category;
+  }
 
-    handleAdd(data: {product: Product, quantity: number}) {
-        this.dataSource.order.addProduct(data.product, data.quantity);
-    }
+  handleAdd(data: {product: Product, quantity: number}) {
+    this.dataSource.order.addProduct(data.product, data.quantity);
+  }
 
-    handleSubmit() {
-        console.log("SUBMIT");
-    }
+  handleSubmit() {
+    this.router.navigateByUrl("/order");
+  }
 }
